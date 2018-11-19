80272bfdae661a3b964c1e87afd1341ff8067ea4
Devtr0n <richard@doublelinepartners.com>
2018-11-18 22:53:57 -0600

    added comment header

--- TWC-WebAPI-Examples.ps1
+++ TWC-WebAPI-Examples.ps1
@@ -1,4 +1,10 @@
-﻿# 1. Actuaries - Occupation Information
+﻿<#
+.Synopsis
+    Real world examples invoking Web API 2 endpoints via Powershell
+.DESCRIPTION
+    These examples invoke TWC LMCI REST-ful APIs
+
+# 1. Actuaries - Occupation Information
 $actuaries = Invoke-WebRequest -Uri "https://texascareercheck.com/api/occupationtexasregionalemploymentinformation?%24filter=SOCCode+eq+%2715-2011+%27+and+RegionNumber+ne+%27000048%27" 
 $actuaries.Content
 
@@ -21,3 +27,13 @@ $airTrafficControllers.Content
 # 6. Air Traffic Controllers - Capital Area, Dallas & Tarrant County Region - (Multiple Occupation Report) - Gap Analysis
 $airTrafficControllers = Invoke-WebRequest -Uri "https://texaslaboranalysis.com/api/GetGapDetails(WDA='14,06,05',CareerCluster=null,NAICSCode=null,STEMEndorsement=null)" -Method "POST" -Body "socCodes=53-2021&socCodeOverrides="
 $airTrafficControllers.Content 
+
+# 7. Brazos Valley Region - Top Statistics
+$brazosValleyTop25 = Invoke-WebRequest -Uri "https://texaslaboranalysis.com/api/GetTop25StatisticsDetail(WDA='16',EducationLevel='0,1,5,2,3,4',ReportType=0,MedianWage=0)"
+$brazosValleyTop25.Content 
+
+# 8. Airline Pilots, CoPilots and Flight Engineers - Tarrant county and Dallas regions - Regional Occupational Profile
+$airlinePilots = Invoke-WebRequest -Uri "https://texaslaboranalysis.com/api/GetRopDetail(WDA%20=%20'05,06',%20SOCCode%20=%20'53-2011')"
+$airlinePilots.Content 
+
+#>
